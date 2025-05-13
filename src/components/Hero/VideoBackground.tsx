0aa55932b6a45b6477c316f7c211ea184cd4aecc
Giancarlol <gian_pc21@hotmail.com>
2025-05-13 11:37:37 +0200

    changes

--- src/components/Hero/VideoBackground.tsx
+++ src/components/Hero/VideoBackground.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 const VideoBackground: React.FC = () => {
   return (
-    <div className="absolute inset-0 z-0">
+    <div className="absolute inset-0 z-0 overflow-hidden">
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="w-full h-full object-cover"
+        className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto transform -translate-x-1/2 -translate-y-1/2"
+        style={{
+          objectFit: 'cover',
+          objectPosition: 'center',
+        }}
       >
         <source
           src="/background-video.mp4"
